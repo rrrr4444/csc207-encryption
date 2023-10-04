3fd4bab6daeed0ffe1406bef00bd951709c30559
rrrr4444 <reedcolloton@gmail.com>
2023-10-03 21:26:58 -0500

    formatting and comets

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,41 +1,46 @@
 import java.io.PrintWriter;
-
+/**
+ * A command line Caesar Cipher.
+ *
+ * @author Reed Colloton
+ * @version 2 of October 2023
+ */
 public class CaesarCipher {
-    public static void main (String[] args) {
-        PrintWriter pen = new PrintWriter(System.out,true);
-        if (args.length != 2) {
-            pen.println("Incorrect number of parameters");
-        } else if (args[0].equals("encode")) {
-            shift(args[1], pen, false);
-        } else if (args[0].equals("decode")) {
-            shift(args[1], pen, true);
-        } else {
-            pen.println(args[0]);
-            pen.println("Valid options are \"encode\" or \"decode\"");
-        } // if
-    } //
+  public static void main(String[] args) {
+    PrintWriter pen = new PrintWriter(System.out, true);
+    if (args.length != 2) {
+      pen.println("Incorrect number of parameters");
+    } else if (args[0].equals("encode")) {
+      shift(args[1], pen, false);
+    } else if (args[0].equals("decode")) {
+      shift(args[1], pen, true);
+    } else {
+      pen.println(args[0]);
+      pen.println("Valid options are \"encode\" or \"decode\"");
+    } // if
+  } // main(String[] args)
 
-    static void shift (String msg, PrintWriter pen, boolean reverse) {
-        int base = "a".toCharArray()[0];
-        for (int key = 0; key < 26; key++) {
-            char[] letters = msg.toCharArray();
-            for (int i = 0; i < letters.length; i++) {
-                int letter_val = letters[i];
-                letters[i] = calculate(letter_val, base, key, reverse);
-            } // for
-            String text = new String(letters);
-            pen.println(text);
-        } // for
-    } //
+  static void shift(String msg, PrintWriter pen, boolean reverse) {
+    int base = "a".toCharArray()[0];
+    for (int key = 0; key < 26; key++) {
+      char[] letters = msg.toCharArray();
+      for (int i = 0; i < letters.length; i++) {
+        int letter_val = letters[i];
+        letters[i] = calculate(letter_val, base, key, reverse);
+      } // for
+      String text = new String(letters);
+      pen.println(text);
+    } // for
+  } // shift(String msg, PrintWriter pen, boolean reverse)
 
-    static char calculate (int letter_val, int base, int key, boolean reverse) {
-        if (reverse) {
-            key *= -1;
-        } // if
-        int char_value = (letter_val - base + key) % 26;
-        if (char_value < 0) {
-            char_value = 26 + char_value;
-        } // if
-        return (char) (char_value + base);
-    } //
+  static char calculate(int letter_val, int base, int key, boolean reverse) {
+    if (reverse) {
+      key *= -1;
+    } // if
+    int char_value = (letter_val - base + key) % 26;
+    if (char_value < 0) {
+      char_value = 26 + char_value;
+    } // if
+    return (char) (char_value + base);
+  } // calculate(int letter_val, int base, int key, boolean reverse)
 } // class CaesarCipher
--- VigenereCipher.java
+++ VigenereCipher.java
@@ -1,40 +1,46 @@
 import java.io.PrintWriter;
-
+/**
+ * A command line Vigenere Cipher.
+ *
+ * @author Reed Colloton
+ * @version 2 of October 2023
+ */
 public class VigenereCipher {
-    public static void main (String[] args) {
-        PrintWriter pen = new PrintWriter(System.out,true);
-        if (args.length != 3) {
-            pen.println("Incorrect number of parameters");
-        } else if (args[0].equals("encode")) {
-            String ciphertext = encode(args[1], args[2], false);
-            pen.println(ciphertext);
-        } else if (args[0].equals("decode")) {
-            String plaintext = encode(args[1], args[2], true);
-            pen.println(plaintext);
-        } else {
-            pen.println("Valid options are \"encode\" or \"decode\"");
-        } // if
-    } //
-    static String encode (String msg, String keyword, boolean reverse) {
-        if (keyword.isEmpty()) {
-            return msg;
-        } // if
-        int base = "a".toCharArray()[0];
-        char[] letters = msg.toCharArray();
-        char[] keys = keyword.toCharArray();
-        for (int i = 0; i < letters.length; i++) {
-            int letter_val = (int) letters[i] - base;
-            int key_val = (int) keys[(i % keys.length)] - base;
-            if (reverse) {
-                key_val *= -1;
-            } // if
-            letter_val = (letter_val + key_val);
-            if (letter_val < 0) {
-                letter_val = 26 + letter_val;
-            } // if
-            letter_val = letter_val % 26;
-            letters[i] = (char) (letter_val + base);
-        } // for
-        return new String(letters);
-    } //
+  public static void main(String[] args) {
+    PrintWriter pen = new PrintWriter(System.out, true);
+    if (args.length != 3) {
+      pen.println("Incorrect number of parameters");
+    } else if (args[0].equals("encode")) {
+      String ciphertext = encode(args[1], args[2], false);
+      pen.println(ciphertext);
+    } else if (args[0].equals("decode")) {
+      String plaintext = encode(args[1], args[2], true);
+      pen.println(plaintext);
+    } else {
+      pen.println("Valid options are \"encode\" or \"decode\"");
+    } // if
+  } // main(String args[])
+
+  static String encode(String msg, String keyword, boolean reverse) {
+    if (keyword.isEmpty()) {
+      return msg;
+    } // if
+    int base = "a".toCharArray()[0];
+    char[] letters = msg.toCharArray();
+    char[] keys = keyword.toCharArray();
+    for (int i = 0; i < letters.length; i++) {
+      int letter_val = (int) letters[i] - base;
+      int key_val = (int) keys[(i % keys.length)] - base;
+      if (reverse) {
+        key_val *= -1;
+      } // if
+      letter_val = (letter_val + key_val);
+      if (letter_val < 0) {
+        letter_val = 26 + letter_val;
+      } // if
+      letter_val = letter_val % 26;
+      letters[i] = (char) (letter_val + base);
+    } // for
+    return new String(letters);
+  } // encode(String msg, String keywords, boolean reverse)
 }
